bee4ea336d8a2c0097cce36644999a7cbccb38a8
js6699415 <js6699415@gmail.com>
2026-02-02 18:05:31 -0500

    Configura settings y archivos de despliegue

--- proyect/settings.py
+++ proyect/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'PROYECT.urls'
+ROOT_URLCONF = 'proyect.urls'
 
 TEMPLATES = [
     {
@@ -59,7 +59,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'PROYECT.wsgi.application'
+WSGI_APPLICATION = 'proyect.wsgi.application'
 
 # BASE DE DATOS
 DATABASES = {
